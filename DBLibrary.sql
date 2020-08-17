24e58407902027267203c612034ab8425934cc98
zahidalidev <engrzahid612@gmail.com>
2020-08-17 11:42:21 +0500

    diagram and sql script of library database

---
+++ DBLibrary.sql
@@ -0,0 +1,81 @@
+USE DBLibrary
+GO
+EXEC sp_changedbowner 'sa'
+
+-- ----------------------------------------------------------------------------
+-- DATABASE DBLibrary
+-- ----------------------------------------------------------------------------
+CREATE DATABASE DBLibrary;
+
+use DBLibrary
+-- ----------------------------------------------------------------------------
+-- Table admin
+-- ----------------------------------------------------------------------------
+CREATE TABLE admin (
+  adminID INT NOT NULL IDENTITY,
+  username VARCHAR(45) NOT NULL,
+  password VARCHAR(45) NOT NULL,
+  PRIMARY KEY (adminID)
+)
+
+-- ----------------------------------------------------------------------------
+-- Table auther
+-- ----------------------------------------------------------------------------
+CREATE TABLE auther (
+  authorID INT NOT NULL IDENTITY,
+  fullname VARCHAR(45) NOT NULL,
+  PRIMARY KEY (authorID)
+)
+
+-- ----------------------------------------------------------------------------
+-- Table publisher
+-- ----------------------------------------------------------------------------
+CREATE TABLE publisher (
+  publisherID INT NOT NULL IDENTITY,
+  fullname VARCHAR(45) NOT NULL,
+  PRIMARY KEY (publisherID)
+)
+
+
+-- ----------------------------------------------------------------------------
+-- Table book
+-- ----------------------------------------------------------------------------
+CREATE TABLE book (
+  bookID INT NOT NULL IDENTITY,
+  name VARCHAR(45) NOT NULL,
+  description VARCHAR(45) NOT NULL,
+  genre VARCHAR(45) NOT NULL,
+  edition VARCHAR(45) NOT NULL,
+  bookImage VARCHAR(45) NOT NULL,
+  price VARCHAR(45) NOT NULL,
+  totalStock VARCHAR(45) NOT NULL,
+  currentStock VARCHAR(45) NOT NULL,
+  authorID INT NOT NULL foreign key references auther(authorID),
+  publisherID INT NOT NULL foreign key references publisher(publisherID),
+  PRIMARY KEY (bookID),
+)
+
+-- ----------------------------------------------------------------------------
+-- Table member
+-- ----------------------------------------------------------------------------
+CREATE TABLE member (
+  memberID INT NOT NULL IDENTITY,
+  password VARCHAR(45) NOT NULL,
+  fullname VARCHAR(45) NOT NULL,
+  mobileNumber VARCHAR(45) NOT NULL,
+  email VARCHAR(45) NOT NULL,
+  streetAddress VARCHAR(45) NOT NULL,
+  city VARCHAR(45) NOT NULL,
+  state VARCHAR(45) NOT NULL,
+  IDCardNumber VARCHAR(45) NOT NULL,
+  PRIMARY KEY (memberID)
+)
+
+-- ----------------------------------------------------------------------------
+-- Table memberbookissue
+-- ----------------------------------------------------------------------------
+CREATE TABLE memberbookissue (
+  memberID INT NOT NULL foreign key references member(memberID),
+  bookID INT NOT NULL foreign key references book(bookID)
+)
+
